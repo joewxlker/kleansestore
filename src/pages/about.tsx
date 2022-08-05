109215bde4cb6fca018fe8ea1657d645847ea0f8
evanf walker <joewxlk3r@gmail.com>
2022-08-05 23:54:41 +0800

    added about

---
+++ src/pages/about.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from "next";
+import Layout from "../components/layout";
+
+const About: NextPage = () => {
+
+    const bool = false;
+    const mobile = false;
+
+    return (
+        <Layout>
+            <>
+            </>
+        </Layout>
+
+    )
+}
+
+export default About
