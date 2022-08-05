08269f4db6b0cbc918f22399b58faeed2fdbab68
evanf walker <joewxlk3r@gmail.com>
2022-08-05 23:55:26 +0800

    added main components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,134 @@
-import type { GetServerSideProps, NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import { FC, useEffect, useState } from "react";
-import { JSONArray, JSONObject } from "superjson/dist/types";
 import Layout from "../components/layout";
-import { trpc } from "../utils/trpc";
+import { useIncrementData } from "../hooks/useIntervals";
+import { inferQueryOutput } from "../utils/trpc";
+import Products from "./products";
 
-interface HomeProps { data: Array<object>, items: Array<object> }
+interface HomeProps { stripe: inferQueryOutput<'stripe.all-products'> }
 interface MainProps { mobile: boolean, bool: boolean }
 interface ShopItems { }
 
-export const getSeverSideProps: GetServerSideProps = async () => {
-  return { props: { data: [{}], items: [{}]} }
+export const getStaticProps: GetStaticProps = async () => {
+  const database = await fetch('http://localhost:3000/api/getStripeItems');
+  const stripe = await database.json();
+  return { props: { stripe } }
 }
 
-const Home: NextPage<HomeProps> = ({data, items}) => {
+const Home: NextPage<HomeProps> = (props) => {
 
   const bool = false;
   const mobile = false;
 
   return (
     <Layout>
-      <div className='' >
-      <header className=''>
-                <h5 className=''>Memberships Available!</h5>
-            </header>
-        <Main mobile={mobile} bool={bool} />
-        <button onClick={e => window.scrollTo(0,0)} > ^ </button>
-      </div>
+      <>
+        <div className='' >
+          <header className=''>
+            <h5 className='' onClick={e => console.log(props)}>Memberships Available!</h5>
+          </header>
+          <Main mobile={mobile} bool={bool} />
+          <ImageCards />
+          <button onClick={e => window.scrollTo(0, 0)} > ^ </button>
+        </div>
+        <Products stripe={props.stripe} />
+      </>
     </Layout>
+
   )
 }
 
 export default Home
 
-export const Main: FC<MainProps> = ({mobile, bool}): JSX.Element => {
+
+interface MainProps { }
+export const Main: FC<MainProps> = (props): JSX.Element => {
+
+  const [count, setCount, setIncrement] = useIncrementData();
+  const [hover, setHover] = useState<boolean>(false);
+
+  const images = [{
+    title: 'KLEANSE  X  HYGIENE',
+    paragraph: "",
+    image: '',
+    active: [true, false, false]
+  },
+  {
+    title: 'KLEANSE AND COFFEE',
+    paragraph: '',
+    image: '',
+    active: [false, true, false]
+  },
+  {
+    title: 'KLEANSE  REWARDS',
+    paragraph: '',
+    image: '',
+    active: [false, false, true]
+  }
+  ]
+  useEffect(() => {
+    if (hover) return;
+    const interval = setInterval(() => {
+      setIncrement(2, 'image-slider', true);
+
+    }, 5000)
+    return () => clearInterval(interval);
+  }, [count, setCount, hover])
+
   return (
-      <>
-          <div className='main-entry-container'>
-          </div>
-          {/* {bool['sidebar'] && mobile && <div className='darken-bg'/>} */}
-      </>
+    <>
+      <div className='' onMouseOver={e => setHover(false)} style={{ position: 'fixed', height: '100vh', width: '100vw', top: '0', zIndex: '0', }} />
+      <div className='' onMouseEnter={e => setHover(true)} style={{ padding: '2rem 0rem', background: 'gray', zIndex: '1' }}>
+        <h1>{images[count['image-slider']]?.title}</h1>
+        <Image src={`${images[count['image-slider']]?.image}`} width={1000} height={500} />
+      </div>
+
+    </>
   );
 }
+
+interface ImageCards { }
+export const ImageCards: FC<ImageCards> = (props): JSX.Element => {
+
+  const [hover, setHover] = useState<boolean>()
+
+  const cards = [{
+    title: "TRENDING",
+    hovertext: 'VIEW PRODUCTS',
+    paragraph: '',
+    image: '',
+    about: ''
+  }, {
+    title: 'NEW ARRIVALS',
+    hovertext: 'VIEW PRODUCTS',
+    paragraph: '',
+    image: '',
+    about: ''
+  }, {
+    title: 'MORE COMING SOON...',
+    hovertext: 'More items coming soon',
+    paragraph: '',
+    image: '',
+    about: ''
+  }]
+
+  return (
+    <>
+      {cards.map((info) => {
+        return (
+
+          <>
+            <div className='' onMouseEnter={e => { e.preventDefault(); setHover(true) }}>
+              <h3 className=''>{info.title}</h3>
+              <p className=''>{info.paragraph}</p>
+              <Image src={info.image} width={200} height={100} />
+
+            </div>
+          </>
+        )
+      })}
+    </>
+  )
+};
