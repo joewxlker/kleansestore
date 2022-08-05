8d832296365af8f6739a42d1ab5e6f740286bfb8
evanf walker <joewxlk3r@gmail.com>
2022-08-05 23:58:06 +0800

    minor changes

--- src/hooks/useIntervals.tsx
+++ src/hooks/useIntervals.tsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react"
+import React, { Dispatch, SetStateAction, useState } from "react"
 
 const useSetCount = () => {
-    const [count, setCounter] = useState<any>({ "iteratePValues": 0, "iterateRepoValues": 0, "repos": 0, "shopSlider": 0 });
+    const [count, setCounter] = useState<any>({ "image-slider": 0 });
     return [count, (data: string, num: number) => {
-        return setCounter((prev: object) => {return{...prev, [data]: num}})
+        return setCounter((prev: object) => { return { ...prev, [data]: num } })
     }]
 }
 export const useIncrementData = () => {
     const [count, setCount] = useSetCount();
-    return [count, setCount, (greater: number, target: string, operator: boolean):React.Dispatch<React.SetStateAction<boolean>> => {
+    return [count, setCount, (greater: number, target: string, operator: boolean): React.Dispatch<React.SetStateAction<boolean>> => {
         //return count/setCount here, no need to import useSetCount outside of useIncrementData
-           
-        if (operator && count[target] < greater)  return setCount(target, (count[target] + 1))
-        if (operator && count[target] >= greater) return setCount(target, 0);
+
+        if (operator && count[target] < greater) return setCount(target, (count[target] + 1))
+        if (operator && count[target] >= greater) return setCount(target, (count[target] - count[target]));
 
         if (!operator && count[target] > greater) return setCount(target, (count[target] - 1))
         if (!operator && count[target] <= greater) return setCount(target, greater)
-           
+
         else return setCount;
     }]
 };
