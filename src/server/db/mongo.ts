bcbfa3cbec8ca8899b11980edbc7cb90a4d2e528
evanf walker <joewxlk3r@gmail.com>
2022-08-05 23:57:45 +0800

    added mongo

---
+++ src/server/db/mongo.ts
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const uri = process.env.MONGO_CLIENT_KEY
+const { MongoClient } = require('mongodb')
+import * as mongoDB from "mongodb";
+
+declare global {
+    var Mongo: mongoDB.MongoClient;
+}
+
+export const mongo: mongoDB.MongoClient = new MongoClient(uri);
