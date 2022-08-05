0df9231e7e5d436e09550cb3219543e2322fc221
evanf walker <joewxlk3r@gmail.com>
2022-08-05 23:57:31 +0800

    removed example route, added mongo, stripe routes

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,11 +2,13 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
+import { mongoDbRouter, stripeRouter } from "./routes";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter);
+  .merge("mongo.", mongoDbRouter)
+  .merge("stripe.", stripeRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
+
