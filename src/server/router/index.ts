6efe047dd8d943cf1e1c0ce2aa8d1aa1e04dd650
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-06 13:50:55 +0800

    added route to sendgrid

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,12 +2,13 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { mongoDbRouter, stripeRouter } from "./routes";
+import { mongoDbRouter, sendgridRouter, stripeRouter } from "./routes";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("mongo.", mongoDbRouter)
   .merge("stripe.", stripeRouter)
+  .merge("sendgrid.", sendgridRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
