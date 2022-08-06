c8471af179279af68dbb55743850ebf7e15545ee
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-06 13:48:02 +0800

    fixed type erros

--- src/hooks/useIntervals.tsx
+++ src/hooks/useIntervals.tsx
@@ -1,22 +1,36 @@
 import React, { Dispatch, SetStateAction, useState } from "react"
 
 const useSetCount = () => {
-    const [count, setCounter] = useState<any>({ "image-slider": 0 });
-    return [count, (data: string, num: number) => {
-        return setCounter((prev: object) => { return { ...prev, [data]: num } })
-    }]
-}
-export const useIncrementData = () => {
-    const [count, setCount] = useSetCount();
-    return [count, setCount, (greater: number, target: string, operator: boolean): React.Dispatch<React.SetStateAction<boolean>> => {
-        //return count/setCount here, no need to import useSetCount outside of useIncrementData
 
-        if (operator && count[target] < greater) return setCount(target, (count[target] + 1))
+    const [count, setCounter] = useState(
+        {
+            image: 0,
+        }
+    );
+    return [count, (data: string, num: number) => {
+        return setCounter((prev) => { return { ...prev, [data]: num } })
+    }] as const
+
+}
+
+export const useIncrementData = () => {
+
+    interface Increment<value> {
+        [target: string]: value
+    }
+
+    const [count, setCount] = useSetCount();
+    return [count, setCount, (greater: number, target: 'image', operator: boolean) => {
+        //return count/setCount here, no need to import useSetCount outside of useIncrementData
+        console.log(greater, target, count, setCount)
+
+        if (operator && count[target] < greater) return setCount(target, (count[target] + 1));
         if (operator && count[target] >= greater) return setCount(target, (count[target] - count[target]));
 
         if (!operator && count[target] > greater) return setCount(target, (count[target] - 1))
         if (!operator && count[target] <= greater) return setCount(target, greater)
 
         else return setCount;
-    }]
+    }] as const
+
 };
