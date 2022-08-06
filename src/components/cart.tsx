8c9398d0abf24da42df54deb563e1636ac4df442
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-06 16:36:21 +0800

    added cart.tsx

---
+++ src/components/cart.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react"
+
+interface CartProps { };
+
+export const Cart: FC<CartProps> = ({ }): JSX.Element => {
+    return (
+        <div className=''>
+        </div>
+    )
+}
