ab51a571e3aa8613540020a45c3fe7a7cd1b4253
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-06 16:39:23 +0800

    made siteInfo.ts, moved data from index

---
+++ src/utils/siteInfo.ts
@@ -0,0 +1,39 @@
+export const images = [{
+    title: 'KLEANSE  X  HYGIENE',
+    paragraph: "",
+    image: '/images/marketing/kleanse_logo_black_no_background.png',
+    active: [true, false, false]
+},
+{
+    title: 'KLEANSE AND COFFEE',
+    paragraph: '',
+    image: '/images/marketing/kleanse_logo_black.jpg',
+    active: [false, true, false]
+},
+{
+    title: 'KLEANSE  REWARDS',
+    paragraph: '',
+    image: '/images/marketing/kleanse_logo_white_no_background.png',
+    active: [false, false, true]
+}
+]
+
+export const cards = [{
+    title: "TRENDING",
+    hovertext: 'VIEW PRODUCTS',
+    paragraph: '',
+    image: '',
+    about: ''
+}, {
+    title: 'NEW ARRIVALS',
+    hovertext: 'VIEW PRODUCTS',
+    paragraph: '',
+    image: '',
+    about: ''
+}, {
+    title: 'MORE COMING SOON...',
+    hovertext: 'More items coming soon',
+    paragraph: '',
+    image: '',
+    about: ''
+}]
