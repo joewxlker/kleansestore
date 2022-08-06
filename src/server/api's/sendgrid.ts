3c71d5001bb6477f24af4b3d7b2fb2ce7f7bf7d1
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-06 13:50:01 +0800

    added sendgrid.ts

---
+++ src/server/api's/sendgrid.ts
@@ -0,0 +1,8 @@
+require('dotenv').config();
+import * as sgMail from "@sendgrid/mail";
+
+declare global {
+    var sendGrid: sgMail.MailService;
+}
+
+export const sendgrid: sgMail.MailService = sgMail;
