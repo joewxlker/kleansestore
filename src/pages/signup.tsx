2000527b448c9e21973fce17abcd93c397847b34
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-07 23:28:44 +0800

    refactoring

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -8,7 +8,9 @@ const Signup: FC<SignupProps> = ({ }): JSX.Element => {
     return (
         <Layout>
             <div className=''>
-                <Form type={['firstname', 'lastname', 'email', 'pass', 'hidden', 'word']} target={'sendGrid'} buttons={['day', 'month', 'year']} onResponse={() => ''} />
+                <Form type={['firstname', 'lastname', 'email', 'password', 'hidden',]} target={'mongo.sign-up'} buttons={['day', 'month', 'year']} onResponse={() => ''} />
+                {/** pass types to create input fields, pass endpoint to target, buttons only needed definition for date of birth elements */}
+                {/** onResponse returns api mutation return value from endpoint @target*  */}
             </div>
         </Layout>
     )
