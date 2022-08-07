0439ecb7ac97845216e4435913a3d19fcc3617c3
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-07 23:30:12 +0800

    added comments

--- src/hooks/useIntervals.tsx
+++ src/hooks/useIntervals.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useState } from "react"
+import { useState } from "react"
 
 const useSetCount = () => {
 
@@ -10,21 +10,24 @@ const useSetCount = () => {
     return [count, (data: string, num: number) => {
         return setCounter((prev) => { return { ...prev, [data]: num } })
     }] as const
-
+    // as const handles types 
 }
 
 export const useIncrementData = () => {
 
     const [count, setCount] = useSetCount();
     return [count, setCount, (greater: number, target: 'image', operator: boolean) => {
+        // update target @types and count/setCount variables for additional instances
 
         if (operator && count[target] < greater) return setCount(target, (count[target] + 1));
         if (operator && count[target] >= greater) return setCount(target, (count[target] - count[target]));
-
+        // increase count values unless target met
         if (!operator && count[target] > greater) return setCount(target, (count[target] - 1))
         if (!operator && count[target] <= greater) return setCount(target, greater)
+        // decrease count values unless target met
 
         else return setCount;
     }] as const
+    // increments / decrements count variables based on input vars 
 
 };
