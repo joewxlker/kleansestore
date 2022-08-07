2edf64df212a8ade284f33ad1ac7a00ed3fd0e1b
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-07 23:28:06 +0800

    added comments

--- src/utils/siteInfo.ts
+++ src/utils/siteInfo.ts
@@ -1,4 +1,4 @@
-
+//this file defines and stores all of the sites data, ready to be mapped to a component at a moments notice 
 export const images = [{
     title: 'KLEANSE  X  HYGIENE',
     paragraph: "",
@@ -17,6 +17,7 @@ export const images = [{
     image: '/images/marketing/kleanse_logo_white_no_background.png',
     active: [false, false, true]
 }
+    // main image slider data
 ]
 
 export const cards = [{
@@ -37,7 +38,9 @@ export const cards = [{
     paragraph: '',
     image: '',
     about: ''
-}]
+}
+    // main image cards data
+]
 
 const generateYears = () => {
     let year = [];
@@ -45,6 +48,7 @@ const generateYears = () => {
         year.push(i);
     }
     return year;
+    // generates years
 }
 const years = generateYears();
 
@@ -52,5 +56,7 @@ export const dateData = {
     day: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
     month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
     year: years
+
+    //used to fill date of birth elements
 }
 
