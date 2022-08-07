12919b307072193c21feb36f59269a4837e4ced3
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-07 11:13:36 +0800

    converted defined obj to type

--- src/hooks/SetForm.tsx
+++ src/hooks/SetForm.tsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 
+interface FormObj {
+  firstname?: '';
+  lastname?: '';
+  email?: '',
+  pass?: '',
+  word?: '',
+  hidden?: '',
+  message?: '',
+  day?: '',
+  month?: '',
+  year?: ''
+}
+
+export const formObj: FormObj = {}
+
 const useSetForm = () => {
-  const [value, setForm] = useState({
-    firstname: '',
-    lastname: '',
-    email: '',
-    password: '',
-    honeypot: '',
-  });
+  const [value, setForm] = useState(formObj);
   return [value, (event: React.ChangeEvent<HTMLInputElement>) => {
     setForm((oldValue) => {
-      return { ...oldValue, [event.target?.name]: event.target.value };
+      return { ...oldValue, [event.target.name]: event.target.value };
     });
   },
-  ];
+  ] as const;
 };
 
 export default useSetForm;
