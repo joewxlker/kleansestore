1a8997b77d5444833464501839f7949bf81a77aa
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-07 23:26:49 +0800

    delete all unnessessary files

--- src/server/api's/sendgrid.ts
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-import * as sgMail from "@sendgrid/mail";
-
-declare global {
-    var sendGrid: sgMail.MailService;
-}
-
-export const sendgrid: sgMail.MailService = sgMail;
--- src/utils/handleLocalData.js
+++
@@ -1,7 +0,0 @@
-export const setLocalData = (type, data) => {   
-    localStorage.setItem(JSON.stringify(type), JSON.stringify(data))
-    return
-}
-export const getLocalData = async (input) => {
-    return await JSON.parse(localStorage.getItem(JSON.stringify(input)));
-}
