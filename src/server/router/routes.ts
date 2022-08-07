6580aa6dd1110de6d35cfe3872781f6cdee1fda1
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-07 23:27:28 +0800

    added routes from old next api files

--- src/server/router/routes.ts
+++ src/server/router/routes.ts
@@ -1,58 +1,158 @@
+import { MailDataRequired } from "@sendgrid/mail";
 import { createRouter } from "./context";
+import { z } from 'zod';
+import * as bcrypt from "bcrypt";
+import { saltRounds } from "../api's/all";
 
 export const mongoDbRouter = createRouter()
-  .query('login', {
+
+  .mutation('login', {
+
+    input: z.object({
+      email: z.string().nullish(),
+      password: z.string().nullish(),
+    }).nullish(),
+
     async resolve({ ctx }) {
+
+      const input = ctx.req?.body[0]
+      //email , password
       await ctx.mongo.connect();
-
       try {
-        const login = await ctx.mongo.db('onlinestore').collection('user_data').findOne({ email: '' });
-        if (login === undefined) return null
-        return true
-      }
+        const login = await ctx.mongo.db('onlinestore').collection('user_data').findOne({ email: input.email });
+        if (login === null) return { result: false }
+        //queries db for matching email and returns false if none found
 
+        const compare = await bcrypt.compare(input.password, login.password);
+        if (!compare) return { result: false }
+        // compares hash stored in db with users password input
+
+        return { result: true }
+        // return true, nextauth handles signin
+      }
       catch (err) {
         console.log(err);
-        return false
+        return { result: false }
       }
+    }
+  })
+
+  .mutation('sign-up', {
+
+    async resolve({ ctx }) {
+
+      const input = ctx.req?.body[0];
+
+      await ctx.mongo.connect();
+      //establish mongo db connection
+      const login = await ctx.mongo.db('onlinestore').collection('user_data').findOne({ email: input.email });
+      // login querries db for document containing query - { email : input.email }
+      if (login !== null) return { result: false }
+      // login = mongo db documument |  null
+      const createHash = async (password: string) => {
+        const hashed = await bcrypt.hash(password, saltRounds)
+        return hashed;
+      }
+
+      const hash = await createHash(input.password);
+      // hash = encrypted password
+      const mongouser = ({ ...input, ['password']: hash });
+      // mongouser = new user object with hash instead of plain text password; 
+      await ctx.mongo.db('onlinestore').collection('user_data').insertOne(mongouser);
+      // inserts new user to DB
+      await ctx.stripe.customers.create({ email: input.email });
+      // creates stripe customer account using input email
+      await ctx.sendgrid.send(
+        {
+          to: process.env.OWNER!,
+          from: process.env.EMAIL!,
+          subject: ` New customer contact email sent from ${ctx.req?.body.email}`,
+          text: `hello, ${ctx.req?.body.message} Sent from Kleanse Website`
+        })
+      // sends verification/welcome email to new user once they have successfully made an account
+
+      return { result: true }
+
     }
   })
 
 export const stripeRouter = createRouter()
+
   .query('all-products', {
     async resolve({ ctx }) {
       const getProducts = async () => {
         const product = await ctx.stripe.products.list();
+        // fetches all products [{}]
         const price = await ctx.stripe.prices.list();
+        // fetches all prices [{}]
         const productData = product.data.map((x, index) => { return { ...x, ...price.data[index] } })
-        return productData;
+        // returns an array of new product obj which includes both product and price key/values
+        return productData
       }
-      return await getProducts()
+      const products = await getProducts();
+      return products;
     }
   })
-  .query('create-checkout', {
-    async resolve({ ctx }) {
 
+  .query('create-checkout-session', {
+    input: z.object({
+      items: z.array(z.string())
+    }),
+
+    async resolve({ ctx }) {
+      const domainURL = process.env.DOMAIN;
+      const session = await ctx.stripe.checkout.sessions.create({
+        mode: 'payment',
+        line_items: ctx.req?.body,
+        success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${domainURL}/canceled.html`,
+      })
+      return { url: session.url };
+      // creates stripe session on stripe servers
+      // returns success/cancel once use makes or cancels a purchase 
+      // the server will render this path
+    }
+  }
+  )
+
+  .mutation('create-account', {
+    async resolve({ ctx }) {
+      const user = await ctx.stripe.customers.create({
+        email: ctx.req?.body.email,
+      });
+      if (user !== null) return { result: true }
+      return { result: false }
+      // returns null if user exists
+      // creates a stripe account from a supplied email. 
     }
   })
 
 export const sendgridRouter = createRouter()
-  .query('send-email', {
+  .mutation('send-email', {
+
+    input: z.object({
+      firstname: z.string(),
+      email: z.string(),
+      message: z.string()
+    }),
+
     async resolve({ ctx }) {
 
-      const msg = {
-        to: '',
-        from: process.env.EMAIL!,
-        subject: 'Please verify your new kleanse online shopping account',
-        text: ''
+      ctx.sendgrid.setApiKey(process.env.SENDGRID_API_KEY!)
+
+      try {
+        const contact: MailDataRequired = {
+          to: process.env.OWNER!,
+          from: process.env.EMAIL!,
+          subject: ` New customer contact email sent from ${ctx.req?.body.email}`,
+          text: `hello, ${ctx.req?.body.message} Sent from Kleanse Website`
+        }
+        //TODO add request params and conditional email templates
+        await ctx.sendgrid.send(contact)
+        return true;
+      } catch (err) {
+        return { result: false }
       }
-      ctx.sendgrid
-        .send(msg)
-        .then(() => {
-          console.log('Email sent')
-        })
-        .catch((error: string) => {
-          console.log(error)
-        })
     }
-  })
+  }
+  )
