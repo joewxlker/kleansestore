da19f8ed9f2194d124bcb459b40669fd5014b11d
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-07 23:30:03 +0800

    refactoring, added comments

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,9 @@
 import NextAuth from "next-auth"
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
-import clientPromise from "../../../lib/mongo"
-
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export default NextAuth({
-    providers: [
-
-    ],
+    providers: [],
+    // no providers specified, using standard auth 
+    debug: true,
 
 })
