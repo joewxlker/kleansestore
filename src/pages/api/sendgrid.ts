9d107e1b0458fe3d4f476522dcb06e1809892514
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-07 11:14:19 +0800

    refactoring

--- src/pages/api/sendgrid.ts
+++ src/pages/api/sendgrid.ts
@@ -2,23 +2,23 @@ import { MailDataRequired } from "@sendgrid/mail";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { sendgrid } from "../../server/api's/sendgrid";
 
-export const sendEmail: NextApiHandler = (Request: NextApiRequest, Response: NextApiResponse) => {
+const sendEmail: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     sendgrid.setApiKey(process.env.SENDGRID_API_KEY!)
-
-    const msg: MailDataRequired = {
-        to: Request.body.email,
-        from: process.env.EMAIL!,
-        subject: 'Please verify your new kleanse online shopping account',
-        text: ''
+    try {
+        const contact: MailDataRequired = {
+            to: 'fabkwi77@gmail.com',
+            from: process.env.EMAIL!,
+            subject: ` New customer contact email sent from ${req.body.email}`,
+            text: `hello, ${req.body.message} Sent from Kleanse Website`
+        }
+        //TODO add request params and conditional email templates
+        const result = await sendgrid.send(contact)
+        res.status(result[0].statusCode).send(true)
+    } catch (err) {
+        res.status(400).send(err)
     }
-    sendgrid
-        .send(msg)
-        .then(() => {
-            console.log('Email sent')
-        })
-        .catch((error: string) => {
-            console.log(error)
-        })
 }
+
+export default sendEmail
 
