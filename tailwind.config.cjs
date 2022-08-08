1a2cc24bc05cec8f60ec4538a84b1fd44fd9126c
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-08 16:40:12 +0800

    updated theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'salmon': '#ffa58f',
+      'grey': '#212121',
+      'grey-light': '#787777',
+      'white': '#ffffff',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
