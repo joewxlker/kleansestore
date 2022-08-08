376c9df49effeb8ecc315cc13769c261e49ab716
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-08 18:28:52 +0800

    added tailwind styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,14 @@ import Layout from "../components/layout";
 import { useIncrementData } from "../hooks/useIntervals";
 import { cards, images } from "../utils/siteInfo";
 import { inferQueryOutput } from "../utils/trpc";
+import Products from '../components/products'
 
 interface HomeProps { products: inferQueryOutput<'stripe.all-products'> }
 /** infer query output infers typing based on routes defined {@module src/server/router/routes.ts} */
 
 export const getStaticProps: GetStaticProps = async () => {
-  const req = await fetch('http://localhost:3000/api/trpc/stripe.all-products');
-  //TODO convert to useMutation
+  const req = await fetch('http://localhost:3000/api/stripe');
+  //TODO convert to useQuery
   const products = await req.json();
   return { props: { products } }
   // populates website with static stripe product data
@@ -19,15 +20,17 @@ export const getStaticProps: GetStaticProps = async () => {
 
 const Home: NextPage<HomeProps> = (props) => {
 
+  console.log(props)
   return (
     <Layout>
       <>
         <div className='' >
-          <header className=''>
+          <header className='z-20 block flex flex-column justify-center bg-salmon h-10 align-center mb-6'>
             <h5 className='' onClick={e => ''}>Memberships Available!</h5>
           </header>
           <Main />
-          <ImageCards />
+          <ImageCards images={props.products} />
+          <Products params={'all-products'} products={props.products} />
           <button onClick={e => window.scrollTo(0, 0)} > ^ </button>
         </div>
       </>
@@ -48,6 +51,9 @@ export const Main: FC<MainProps> = (): JSX.Element => {
   // prevents setInterval from running on hover
 
   useEffect(() => {
+    console.log(hover)
+  })
+  useEffect(() => {
     if (hover) return;
     const interval = setInterval(() => {
       setIncrement(2, 'image', true);
@@ -59,39 +65,39 @@ export const Main: FC<MainProps> = (): JSX.Element => {
 
   return (
     <>
-      <div className='' onMouseOver={e => setHover(false)} style={{ position: 'fixed', height: '100vh', width: '100vw', top: '0', zIndex: '-1', background: 'rgba(0,0,0,0.1)' }} />
-      <div className='' onMouseEnter={e => setHover(true)} style={{ padding: '2rem 0rem', background: 'gray', zIndex: '1' }}>
+      <div className='bg-grey' onMouseEnter={e => setHover(false)} onMouseLeave={e => setHover(false)} style={{ position: 'fixed', height: '100vh', width: '100vw', top: '0', zIndex: '0' }} />
+      <div className='' onMouseEnter={e => setHover(true)} style={{ margin: '3rem 0rem', background: 'gray', zIndex: '1' }}>
         <h1>{images[count['image']]?.title}</h1>
         <Image src={`${images[count['image']]?.image}`} width={2000} height={700} alt={``} />
       </div>
 
     </>
   );
-  {/** images imported from @module src/utils/siteInfo.ts */ }
+  {/** images imported from src/utils/siteInfo.ts */ }
 }
 
-interface ImageCards { }
+interface ImageCards { images: Array<object> }
 export const ImageCards: FC<ImageCards> = (props): JSX.Element => {
 
   const [hover, setHover] = useState<boolean>()
   // hover target image adds effects
 
   return (
-    <>
-      {cards.map((info) => {
+    <div className='w-full flex flex-row flex-nowrap flow-scroll z-1 relative'>
+      {cards.map((info, key) => {
         return (
 
           <>
-            <div className='' onMouseEnter={e => { e.preventDefault(); setHover(true) }}>
+
+            <div className=' w-2/4' onMouseEnter={e => { e.preventDefault(); setHover(true) }}>
               <h3 className=''>{info.title}</h3>
               <p className=''>{info.paragraph}</p>
-              <Image src={info.image} width={600} height={320} alt={''} />
-
+              <div className='w-90 h-80 bg-salmon z-3' style={{ backgroundImage: 'url("https://files.stripe.com/links/MDB8YWNjdF8xTEtIM1lFdTkwczBGNXpJfGZsX3Rlc3RfOEZvOU9XejJJRHV2NDBjS2dYTjdwSGtz00AYcNJHih")' }} />
             </div>
           </>
         )
       })}
-      {/** images imported from @module src/utils/siteInfo.ts */}
-    </>
+      {/** images imported from src/utils/siteInfo.ts */}
+    </div>
   )
 };
