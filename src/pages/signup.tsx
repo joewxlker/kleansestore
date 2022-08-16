b067c9af4f3ea6b405906087ca40f67db2701531
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-16 15:36:07 +0800

    changed types prop to formDataObj prop, moved trpc request from form component to signup

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,16 +1,28 @@
 import { FC } from "react"
 import { Form } from "../components/form"
 import Layout from "../components/layout"
+import { FormType, SignUpForm } from "../hooks/SetForm";
+import { client } from "./_app";
 
 interface SignupProps { };
 
 const Signup: FC<SignupProps> = ({ }): JSX.Element => {
+
+    const handleRequest = async (data: FormType<SignUpForm>) => {
+        const res = await client.mutation('mongo.sign-up', data);
+        console.log(res)
+    }
+
     return (
         <Layout>
             <div className=''>
-                <Form type={['firstname', 'lastname', 'email', 'password', 'hidden',]} target={'mongo.sign-up'} buttons={['day', 'month', 'year']} onResponse={() => ''} />
-                {/** pass types to create input fields, pass endpoint to target, buttons only needed definition for date of birth elements */}
-                {/** onResponse returns api mutation return value from endpoint @target*  */}
+                <Form
+                    formData={{ firstname: '', lastname: '', email: '', password: '', hidden: '' }}
+                    target={'mongo.sign-up'}
+                    buttons={['day', 'month', 'year']}
+                    onResponse={handleRequest} />
+                {/** pass types to create input fields, pass endpoint to target, buttons only needs definition for date of birth elements */}
+                {/** onResponse returns complete form obj */}
             </div>
         </Layout>
     )
