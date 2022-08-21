484e2afe6390152c49b8f899e4c36e6c45c6b242
riectivenoodes <joewxlk3r@example.com>
2022-08-21 14:26:55 +0800

    fixed Form props to meet recent changes, added response function

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,15 +1,18 @@
 import { FC } from "react"
 import { Form } from "../components/form";
 import Layout from "../components/layout"
-
-export interface ContactForm extends FormData { }
+import { ContactForm, FormType } from "../hooks/SetForm";
 
 const Contact: FC = ({ }): JSX.Element => {
+
+    const handleResponse = (data: FormType<ContactForm>) => {
+        return
+    }
 
     return (
         <Layout>
             <div className=''>
-                <Form type={['firstname', 'email', 'hidden', 'message']} target={'sendgrid.send-email'} buttons={[]} onResponse={(data) => console.log(data)} />
+                <Form formData={{ firstname: '', email: '', hidden: '', message: '' }} target={'sendgrid.send-email'} buttons={[]} onResponse={handleResponse} />
                 {/* contact form, doesnt require date of birth elements, pass empty array */}
                 {/* onResponse returns boolean from api */}
             </div>
