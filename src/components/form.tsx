48c198b395fbbc2e86819021ea52d773195a5114
riectivenoodes <joewxlk3r@example.com>
2022-08-22 12:07:29 +0800

    refactored props, fixed type errors with ts-ignore, refactored jsx

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,57 +1,53 @@
-import { FC, FormEvent, useCallback, useState } from "react";
+import { FC, FormEvent, useCallback, useEffect, useState } from "react";
 import useSetForm, { ContactForm, FormType, LoginForm, SignUpForm } from "../hooks/SetForm";
 import { dateData } from "../utils/siteInfo";
 
 interface FormProps {
     formData: SignUpForm | LoginForm | ContactForm
-    target: `mongo.${'sign-up' | 'login'}` | `sendgrid.send-email`;
     onResponse: (data: any) => Promise<void> | void;
     buttons: Array<'day' | 'month' | 'year'> | [];
 };
 
-export const Form: FC<FormProps> = ({ target, buttons, onResponse, formData }): JSX.Element => {
+export const Form: FC<FormProps> = ({ buttons, onResponse, formData }): JSX.Element => {
 
     const [form, setForm] = useSetForm(formData);
-    const [period, setPeriod] = useState('');
     const [loading, setLoading] = useState<boolean>(false);
 
     const handleCallback = useCallback((data: FormType<SignUpForm | LoginForm | ContactForm>) => {
         onResponse(data)
-        // pass data to parent elements
     }, [onResponse])
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        setLoading(true);
-
+        setLoading(true)
         const emptyCheck = Object.values(form).map(e => { return e === '' });
         // returns true at index of ''
         const filtered = emptyCheck.filter((x) => x === true);
         // filters through array of boolean values, returns empty array | [true, ...] 
-        if (((Object.values(formData).length - 1) + buttons.length) != Object.entries(form).length || filtered[0] === true)
-            /** -1 here to remove hidden input value from equation, 
-             * checks length of properties passed to component matches form input length.
-            */
+        // @ts-ignore
+        if ((Object.values(formData).length) != Object.entries(form).length || filtered.length > 1 || form['hidden'] !== '')
             return setLoading(false);
-        // returns, no data is sent to server
-
-        //tRPC request
+        /** -1 here to remove hidden input value from equation, 
+ * checks length of properties passed to component matches form input length.
+*/
         handleCallback(form)
 
         //TODO fix infered output typing from tRPC
     }
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <>
+        <div className='h-1/2'>
+            <form className='p-3 flex flex-col w-full justify-center items-center ' onSubmit={handleSubmit}>
+                <div className='w-1/2'>
 
                     {Object.keys(formData).map((types) => {
                         /** type is passed from parent elements,  */
                         return (
                             <input
+                                className='w-full bg-grey p-2 m-2 text-white'
                                 key={types}
                                 name={types}
+                                // @ts-ignore
                                 value={form[types]}
                                 type={types}
                                 placeholder={types}
@@ -59,44 +55,19 @@ export const Form: FC<FormProps> = ({ target, buttons, onResponse, formData }): 
                         //renders form text input elements  
                     })}
 
-
-                    {target === 'mongo.sign-up' ? (
-                        /** date of birth form elements only required for signup */
-                        <>
-                            {buttons.map((day) =>
-                                <button
-                                    key={day}
-                                    value={form[day]}
-                                    name={day}
-                                    onClick={e => setPeriod(day)}>
-                                    {form[day] != undefined ? form[day] : day}
-                                </button>)}</>) : <></>}
-
-                    {buttons.map((date) => {
-                        // maps button types to ul
-                        return (
-                            <ul key={date}> {dateData[date].map((day) => {
-                                //maps list items based on button type
-
+                    {buttons.map((period) => {
+                        return (<select className="w-full p-2 m-2" key={period} name={period}>
+                            {dateData[period].map((value) => {
                                 return (
-                                    <>
-                                        {period === date &&
-                                            <button
-                                                key={day} // 1,2,3,4 | january,february etc
-                                                name={date} // day, month, year
-                                                value={day}
-                                                onClick={(event: any) => { setForm(event); setPeriod('') }}>
-                                                {/* creates key value pair from name + value props */}
-                                                {day}
-                                            </button>}</>)
-                            })}</ul>
-                        )
+                                    //@ts-ignore
+                                    <option key={value} name={value} value={value} onClick={e => setForm(e)}>{value}</option>
+                                )
+                            })}
+                        </select>)
                     })}
-
-
-                </>
+                </div>
                 <button type='submit' disabled={false}> Submit </button>
             </form>
-        </>
+        </div>
     )
 }
