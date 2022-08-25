8ab9d269b59515eda29bebd8d82bd58f8ab20203
riectivenoodes <joewxlk3r@example.com>
2022-08-25 21:42:54 +0800

    nested [products] inside products folder

---
+++ src/pages/products/[products].tsx
@@ -0,0 +1,37 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { FC } from "react";
+import Layout from "../../components/layout";
+import Products from "../../components/products";
+import { inferQueryOutput } from "../../utils/trpc";
+
+interface ProductsProps {
+    products: inferQueryOutput<'stripe.all-products'>
+    params: string;
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const req = await fetch('http://localhost:3000/api/stripe');
+    //TODO convert to useQuery
+    const products = await req.json();
+    const params = context.params?.products
+    return { props: { products, params } }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [{ params: { products: 'all-products' } }],
+        fallback: false
+    }
+}
+
+const ProductsPage: FC<ProductsProps> = (props) => {
+    return (
+        <Layout>
+            <div className="w-screen h-screen pt-40">
+                <Products params={props.params} products={props.products} />
+            </div>
+        </Layout>
+    )
+}
+
+export default ProductsPage
