d818370767a4bbd8cb4ecd698dc29371f2f3424a
riectivenoodes <joewxlk3r@example.com>
2022-08-26 15:37:41 +0800

    fixed type errors and added edge casing for undefined/null data

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,6 +1,8 @@
+import { Session } from "next-auth";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { FC, useCallback, useState } from "react";
+import Stripe from "stripe";
 import { client } from "../pages/_app";
 import { inferQueryOutput } from "../utils/trpc";
 
@@ -15,8 +17,8 @@ const Products: FC<ShopProps> = (props): JSX.Element => {
     const [overlay, openOverlay] = useState(false);
     const [productData, setProductData] = useState<ProductData>();
 
-    const handleClick = (images: string, name: string, amount: number, price: string) => {
-        setProductData({ images: images, name: name, amount: amount, default_price: price })
+    const handleClick = (images: string, name: string, amount: number, price: string | Stripe.Price, quantity: number) => {
+        setProductData({ image: images, name: name, amount: amount, default_price: JSON.stringify(price), quantity: quantity });
         openOverlay(true);
     }
 
@@ -28,12 +30,42 @@ const Products: FC<ShopProps> = (props): JSX.Element => {
                     return (
                         <>
                             {data.metadata.category !== props.params && <div key={data.name} className='w-80 m-6 flex flex-col bg-white'>
-                                <div className='w-full hover:blur cursor-pointer' onClick={e => handleClick(data.images[0], data.name, data.unit_amount! / 100, data.default_price)}>
+                                <button className='w-full hover:blur cursor-pointer' onClick={e => {
+                                    if (data.default_price === undefined || data.default_price === null) return;
+                                    const images = data.images[0] !== undefined ? data.images[0] : '';
+                                    handleClick(images, data.name, data.unit_amount! / 100, data.default_price, 1)
+                                    // if no image url provided sets image to empty string
+                                    // if no price provided, disables usage
+                                }
+                                }
+                                >
                                     <Image alt='' src={data.images[0]!} height={350} width={400} />
-                                </div>
+                                </button>
                                 <h2>{data.name}</h2>
                                 {data.unit_amount! / 100}
-                                <button className='' onClick={e => handleCartUpdates({ image: data.images[0], price: data.default_price, quantity: 1 }, session)}>Add to cart</button>
+                                <button
+                                    className=''
+                                    onClick={e => {
+                                        if (data.default_price === undefined || data.default_price === null) return;
+                                        const images = data.images[0] !== undefined ? data.images[0] : '';
+                                        const amount = data.unit_amount !== undefined ? data.unit_amount : 0;
+                                        const finalAmount = amount !== null ? amount : 0;
+                                        // if no image url provided sets image to empty string
+                                        // if no unit_amount provided, sets unit amount to 0 on client side
+                                        handleCartUpdates(
+                                            {
+                                                image: images,
+                                                name: data.name,
+                                                amount: finalAmount,
+                                                default_price: JSON.stringify(data.default_price),
+                                                quantity: 1
+                                            }, session)
+                                    }
+                                    }
+                                    disabled={
+                                        data.default_price === undefined || data.default_price === null
+                                    }
+                                >Add to cart</button>
                             </div>}
                         </>
                     )
@@ -48,10 +80,11 @@ const Products: FC<ShopProps> = (props): JSX.Element => {
 export default Products
 
 interface ProductData {
-    images: string;
+    image: string;
     name: string;
     amount: number;
     default_price: string;
+    quantity: number;
 }
 interface ProductOverlayProps {
     onCloseOverlay: () => void;
@@ -75,11 +108,20 @@ export const ProductOverlay: FC<ProductOverlayProps> = ({ onCloseOverlay, data }
                 <div className='z-1 relative w-full h-full flex flex-col'>
                     <div key={data.name} className='w-80 m-5 flex flex-col bg-white'>
                         <div className='w-full h-full cursor-pointer'>
-                            <Image alt='' src={data.images[0]!} height={350} width={400} />
+                            <Image alt='' src={data.image !== undefined ? data.image : ''} height={350} width={400} />
                         </div>
                         <h2>{data.name}</h2>
                         {data.amount}
-                        <button className='' onClick={e => handleCartUpdates({ image: data.images[0], price: data.default_price, quantity: 1 }, session)}>Add to cart</button>
+                        <button className='' onClick={e => {
+                            handleCartUpdates(
+                                {
+                                    image: data.image,
+                                    default_price: data.default_price,
+                                    quantity: data.quantity, name: data.name,
+                                    amount: data.amount
+                                }, session)
+                        }
+                        }>Add to cart</button>
                     </div>
                 </div>
             </div>
@@ -87,29 +129,22 @@ export const ProductOverlay: FC<ProductOverlayProps> = ({ onCloseOverlay, data }
     )
 }
 
-export interface CartItem {
-    image: string;
-    item: string;
-    price: string;
-    quantity: number;
-}
-
-export const handleCartUpdates = async (item: CartItem, session: string) => {
+export const handleCartUpdates = async (item: ProductData, session: Session | null) => {
 
     if (session !== null) {
-        const res = client.mutation('mongo.mongo-carts', { session: session, item: item });
-        return console.log(res)
+        //TODO use session to validate user email, asign cart items to user email. 
+        const res = client.mutation('mongo.mongo-carts', { session: JSON.stringify(session), item: item });
     }
     return setLocalStorage(item)
 
     //sends cart data to db if sesison detected, else uses local storage
 }
 
-export const setLocalStorage = (data: CartItem) => {
+export const setLocalStorage = (data: ProductData) => {
 
     const localCart = window.localStorage.getItem('cart');
     if (localCart !== null) {
-        const filtered = JSON.parse(localCart).filter((x: CartItem) => x.price === data.price);
+        const filtered = JSON.parse(localCart).filter((x: ProductData) => x.default_price === data.default_price);
         if (filtered.length !== 0) return
         window.localStorage.setItem('cart', JSON.stringify([...JSON.parse(localCart), data]));
         return
