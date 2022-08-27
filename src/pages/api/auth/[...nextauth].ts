70809ef7aee41302506ff54d1c948e34aa032497
riectivenoodes <joewxlk3r@example.com>
2022-08-27 19:55:50 +0800

    added credentials provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,32 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
-export default NextAuth({
-    providers: [],
-    // no providers specified, using standard auth 
-    debug: true,
 
-})
+export const authOptions: NextAuthOptions = {
+
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: 'email', type: 'text', placeholder: 'email...' },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials, req) {
+                const user = { email: credentials?.email };
+                console.log(req.body)
+                if (user) {
+                    return (user)
+                }
+                return null
+            }
+        })
+    ],
+
+
+    secret: 'IamVeryHandsome',
+    debug: true,
+    // no providers specified, using standard auth 
+}
+
+export default NextAuth(authOptions)
