1d71f80401781531554c4d252f078d76574c98ca
riectivenoodes <joewxlk3r@example.com>
2022-08-27 19:51:46 +0800

    fixed next auth / login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -10,20 +10,23 @@ export const Login: FC<LoginProps> = ({ }): JSX.Element => {
 
     const [errer, setError] = useState<string | null>(null);
     const { data: session } = useSession()
-    // using session here to render login / logout
 
     const handleRequest = async (data: FormType<LoginForm>) => {
-        const res = await client.mutation('mongo.login', data);
-        res.result ? signIn() : setError('INVALID INPUT');
+        const res = await client.mutation('mongo.login', { email: data.email, password: data.password })
+        res.result ? handleSuccess(data.email) : setError('INVALID INPUT');
+    }
+    const handleSuccess = async (email: string) => {
+        signIn(email)
+        // login binds session to document which can then be used to access document without user input
     }
 
-    // if (session) {
-    //     return (
-    //         <>
-    //             <button onClick={e => signOut()}>Log out</button>
-    //         </>
-    //     )
-    // }
+    if (session) {
+        return (
+            <>
+                <button onClick={e => signOut()} className='px-3 py-2 bg-white'>Log out</button>
+            </>
+        )
+    }
     return (
         <>
             <Link href={'/signup'}><a><p className=' relative bottom-0 text-white text-center'>Don&#39;t have an account? Sign up here</p></a></Link>
