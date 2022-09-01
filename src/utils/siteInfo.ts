2ae1948ad59a7c8bd24eb7eaddd937746684f093
riectivenoodes <joewxlk3r@example.com>
2022-09-01 17:44:08 +0800

    refactoring before build

--- src/utils/siteInfo.ts
+++ src/utils/siteInfo.ts
@@ -1,28 +1,34 @@
-//this file defines and stores all of the sites data, ready to be mapped to a component at a moments notice 
-export const images = [{
-    title: 'KLEANSE  X  HYGIENE',
-    paragraph: "",
-    buttonText: 'READ MORE',
-    href: '',
-    image: '/images/models/womenwithflower.jpg',
-    active: [true, false, false]
-},
-{
-    title: 'KLEANSE X TEA',
-    paragraph: '',
-    buttonText: 'VIEW PRODUCTS',
-    href: '',
-    image: '/images/models/womenputtingcream.jpg',
-    active: [false, true, false]
-},
-{
-    title: 'KLEANSE  REWARDS',
-    paragraph: '',
-    buttonText: 'SAVE NOW',
-    href: '',
-    image: '/images/models/womenwithbrush.jpg',
-    active: [false, false, true]
+//this file defines and stores all of the sites data, ready to be mapped to a component at a moments notice
+
+interface Images {
+    title: string;
+    paragraph: string;
+    buttonText: string;
+    href: string;
+    image: string;
+    active: Array<boolean>;
+    id: number;
 }
+
+export const images: Array<Images> = [
+    {
+        title: 'KLEANSE X TEA',
+        paragraph: '',
+        buttonText: 'VIEW PRODUCTS',
+        href: '/products/all-products',
+        image: '/images/models/womenholdingbottle.jpg',
+        active: [false, true, false],
+        id: 1,
+    },
+    {
+        title: 'KLEANSE  REWARDS',
+        paragraph: '',
+        buttonText: 'SAVE NOW',
+        href: '/prodcts/all-products',
+        image: '/images/models/womenwithbrush.jpg',
+        active: [false, false, true],
+        id: 2,
+    }
     // main image slider data
 ]
 
@@ -30,20 +36,23 @@ export const cards = [{
     title: "TRENDING",
     hovertext: 'VIEW PRODUCTS',
     paragraph: '',
-    image: '',
-    about: ''
+    image: '/images/models/tincturebluebg.jpg',
+    about: '',
+    alt: '',
 }, {
     title: 'NEW ARRIVALS',
     hovertext: 'VIEW PRODUCTS',
     paragraph: '',
-    image: '',
-    about: ''
+    image: '/images/models/tincturebluebg.jpg',
+    about: '',
+    alt: '',
 }, {
     title: 'MORE COMING SOON...',
     hovertext: 'More items coming soon',
     paragraph: '',
-    image: '',
-    about: ''
+    image: '/images/models/tincturebluebg.jpg',
+    about: '',
+    alt: '',
 }
     // main image cards data
 ]
