b3d1a609f5213abb640fad9b1334fcd51900e928
riectivenoodes <joewxlk3r@example.com>
2022-09-01 17:42:41 +0800

    added custom document for csp & nonce

---
+++ src/pages/_document.tsx
@@ -0,0 +1,49 @@
+import { DocumentContext, loadGetInitialProps, NextPageContext } from "next/dist/shared/lib/utils";
+import Document, { Html, Main, NextScript, Head } from "next/document";
+import Script from "next/script";
+import { generateNonce } from "../utils/generateCsp";
+import generateCSP from "../utils/generateCsp";
+
+interface DocumentProps {
+    nonce: string;
+}
+
+
+export default class MyDocument extends Document<DocumentProps> {
+
+    static async getInitialProps(ctx: DocumentContext) {
+
+        const nonce: string = generateNonce();
+        ctx.res?.setHeader('Content-Security-Policy', generateCSP({ nonce }));
+        const initialProps = await Document.getInitialProps(ctx);
+        const additionalProps = { nonce };
+        return {
+            ...initialProps,
+            ...additionalProps,
+        };
+
+    }
+
+    render(): JSX.Element {
+        const { nonce } = this.props
+        return (
+            <Html lang='en'>
+                <Head>
+                    <Script
+                        id={nonce}
+                        nonce={nonce}
+                        dangerouslySetInnerHTML={{
+                            __html: `window.__webpack_nonce__ = "${nonce}"`
+                        }}
+                    />
+
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
