3d09e77751b86096924929638de77596f3fcf581
riectivenoodes <joewxlk3r@example.com>
2022-09-01 17:39:37 +0800

    removed login form,  next auth callback handles credentials, db look up etc

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -8,31 +8,28 @@ import { Form } from "./form";
 interface LoginProps { }
 export const Login: FC<LoginProps> = ({ }): JSX.Element => {
 
-    const [errer, setError] = useState<string | null>(null);
     const { data: session } = useSession()
-
-    const handleRequest = async (data: FormType<LoginForm>) => {
-        const res = await client.mutation('mongo.login', { email: data.email, password: data.password })
-        res.result ? handleSuccess(data.email) : setError('INVALID INPUT');
-    }
-    const handleSuccess = async (email: string) => {
-        signIn(email)
-        // login binds session to document which can then be used to access document without user input
-    }
 
     if (session) {
         return (
-            <>
+            <div className='h-1/2 w-full flex flex-col justify-evenly items-center'>
+                <h1 className='text-white'> Welcome back {session.user?.email}</h1>
                 <button onClick={e => signOut()} className='px-3 py-2 bg-white'>Log out</button>
-            </>
+            </div>
         )
     }
     return (
         <>
             <Link href={'/signup'}><a><p className=' relative bottom-0 text-white text-center'>Don&#39;t have an account? Sign up here</p></a></Link>
-            <Form formData={{ email: '', password: '', hidden: '' }} buttons={[]} onResponse={handleRequest} />
-            {errer !== null && <h1 className='text-white text-center'>{errer}</h1>}
+            <button onClick={e => signIn()} className='px-5 py-3 text-black bg-white'>LOGIN</button>
         </>
     )
 
+}
+
+
+export const Forgotten: FC = (): JSX.Element => {
+    return (
+        <p className='text-white'>Forgotten Password?</p>
+    )
 }
